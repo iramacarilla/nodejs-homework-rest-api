986f1962df1ef84fd7a26522a28263299394139a
Iryna Kosynienko <ikosynienko@gmail.com>
2021-04-13 09:50:40 +0200

    HW-6

--- model/users.js
+++ model/users.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
-const jimp = require('jimp')
+const jimp = require('jimp');
+const { nanoid } = require('nanoid');
 const User = require('../schemas/userSchema')
+const sgMail = require('@sendgrid/mail')
+const Mailgen = require('mailgen')
+
+//const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
 
 const findById = async id => {
   const user = await User.findById(id)/*{_id: id}*/
@@ -13,10 +18,11 @@ const findByEmail = async email => {
     return user
   }
 
-  const createNewUser = async ({email, password, subscription, token}) => {
-      const user = await new User({email, password, subscription, token}).save()
+  /*const createNewUser = async ({email, password, subscription, token}) => {
+    const verificationToken = nanoid()
+      const user = await new User({email, password, subscription, token, verificationToken }).save()
       return user
-  }
+  }*/
   const updateToken = async (id, token) => {
      const user = await User.updateOne({_id: id}, {token})
     return user
@@ -29,14 +35,78 @@ const findByEmail = async email => {
     const user = await User.findByIdAndUpdate({ _id: id}, {avatarURL},  { new: true })
     return user
   }
+  const verifyUser = async ({verificationToken}) => {
+    console.log(verificationToken);
+    const user = await User.findOne({verifyToken: verificationToken})
+    if(user) {
+      await user.updateOne({verifyToken: null, verify: true})
+      return true
+    }
+    return false
+  }
   
+  const createTemplate = async ({verifyToken, username}) => {
+    const mailGenerator = await new Mailgen({
+      theme: 'default',
+      product: {
+          name: 'ContactsSystem',
+          link: 'http://localhost:3000/'
+      }
+  });
+  
+  const email =  {
+    body: {
+        name: username,
+        intro: 'Welcome to ContactsSystem! We\'re very excited to have you on board.',
+        action: {
+            instructions: 'To get started with ContactsSystem, please click here:',
+            button: {
+                color: '#22BC66', 
+                text: 'Confirm your account',
+                link: `http://localhost:3000/api/users/verify/${verifyToken}`
+            }
+        },
+        outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+    }
+  
+};
+console.log(`http://localhost:3000/api/users/verify/${verifyToken}`)
+const emailBody = await mailGenerator.generate(email);
+return emailBody
+  }
+
+const sendEmail = async ({verifyToken, username, email}) => {
+  const emailBody = await createTemplate({verifyToken, email, username})
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg =  {
+  to: email,
+  from: 'ikosynienko@gmail.com', // Use the email address or domain you verified above
+  subject: 'Sending with Twilio SendGrid is Fun',
+  html: emailBody ,
+};
+console.log(email);
+//ES6
+await sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
+//ES8
+
+} 
 
   module.exports = {
     findById,
     findByEmail,
-    createNewUser,
+    //createNewUser,
     updateToken,
     updateUser,
     updateAvatar,
+    verifyUser,
+    sendEmail
  
   }
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,10 @@ const userControllers = require('../../controllers/users');
 const guard = require('../../helpers/guard')
 const upload = require('../../helpers/multer')
 
+
+
+router.get('/verify/:verificationToken', userControllers.verify) 
+router.post('/verify', userControllers.resending) 
 router.get('/current', guard, userControllers.current) 
 router.patch('/', guard, userControllers.updateSub) 
 router.patch("/avatars", guard, upload.single('avatar'), userControllers.avatars)
